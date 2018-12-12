22efb5ba1470eb54cb6fcf3b439a4fcef45efba2
lizardman2297 <45826441+lizardman2297@users.noreply.github.com>
2018-12-13 00:14:12 +0100

    add css index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SIO</title>
+    <link rel="stylesheet" type="text/css" href="public/css/base.css" />
+    <link rel="stylesheet" type="text/css" href="public/css/menu.css" />
+</head>
+<body>
+    
+</body>
+</html>
